ac737102ac40c6c38f1ca4bf4c5be5594f9ee6d2
Andrew Tsukuda <andrew@Andrews-MacBook-Pro-2.local>
2020-02-09 11:27:35 -0800

    BaseKit Init

---
+++ BaseKit/BaseKit.h
@@ -0,0 +1,19 @@
+//
+//  BaseKit.h
+//  BaseKit
+//
+//  Created by Andrew Tsukuda on 2/9/20.
+//  Copyright © 2020 Andrew Tsukuda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseKit.
+FOUNDATION_EXPORT double BaseKitVersionNumber;
+
+//! Project version string for BaseKit.
+FOUNDATION_EXPORT const unsigned char BaseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseKit/PublicHeader.h>
+
+
